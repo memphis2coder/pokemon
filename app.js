2faac978e87f2d4e977a09917e1ee4e497bd54e1
Antonio <Memphis2Coder@gmail.com>
2020-04-13 19:36:36 -0400

    display pokemons

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
+const pokedex = document.getElementById('pokedex');
+const query = document.getElementById('search').value;
+
 function fetchPokemon() {
-    const query = document.getElementById('search').value;
-    const url = `https://pokeapi.co/api/v2/pokemon/${query}`
-    fetch(url)
-        .then(response =>
-            response.json())
-        .then((data) => {
-            console.log(data);
-            const pokemon = {};
-            pokemon['name'] = data.name;
-            pokemon['id'] = data.id;
-            pokemon['image'] = data.sprites['front_default']
-            console.log(pokemon)
-        })
-        .catch((error) => {
-            console.log('Error:', error)
-        })
+    const promises = [];
+    for (let i = 1; i <= 15; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((response) => response.json()));
+    }
+
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map(type => type.type.name).join(', ')
+        }));
+        displayPokemon(pokemon)
+    });
 };
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map(pokeman => `
+    <li class=card>
+        <img src = "${pokeman.image}" />
+        <div class=container>
+            <h2>${pokeman.id}. ${pokeman.name}</h2>
+            <p>Type : ${pokeman.type}</p>
+        </div>
+    </li>
+    `).join("");
+
+    pokedex.innerHTML = pokemonHTMLString;
+}
 
 fetchPokemon();
